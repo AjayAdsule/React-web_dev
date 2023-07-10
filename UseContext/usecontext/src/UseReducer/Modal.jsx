af40c3f1f0679bd58e2dd59fbb4b6466afc702da
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-07-10 11:25:38 +0530

    UseReducer

--- UseContext/usecontext/src/UseReducer/Modal.jsx
+++ UseContext/usecontext/src/UseReducer/Modal.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const Modal = ({modalContent, modalTextColor}) => {
+const Modal = ({ modalContent, modalTextColor, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
   return (
     <React.Fragment>
-      <h2 className={modalTextColor}>{modalContent}</h2>
+      <h2 className={modalTextColor} style={{ textAlign: "center" }}>
+        {modalContent}
+      </h2>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
---
+++ UseContext/usecontext/src/UseReducer/UsingReducer.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useReducer } from "react";
+import { Data } from "../Data";
+import Modal from "./../UseReducer/Modal";
+
+const reducerFunction = (state, action) => {
+  if (action.type === "test") {
+    const newItems = [...state.people, action.payLoad];
+
+    return {
+      ...state,
+      modalOpen: "true",
+      modalContent: "item added",
+      modalTextColor: "green",
+      people: newItems,
+    };
+  }
+  if (action.type === "noValue") {
+    return {
+      ...state,
+      modalContent: "please add item",
+      modalTextColor: "red",
+      modalOpen: "true",
+    };
+  }
+  if (action.type === "remove") {
+    const filterItems = state.people.filter(
+      (item) => item.id !== action.payLoad
+    );
+    return {
+      ...state,
+      people: filterItems,
+      modalContent: "Item remove ",
+      modalTextColor: "black",
+      modalOpen: true,
+    };
+  }
+  if (action.type === "closeModal") {
+    return {
+      ...state,
+      modalOpen: false,
+    };
+  }
+
+  return state;
+};
+const initialValue = {
+  modalOpen: true,
+  people: Data,
+  modalContent: "Use reducer",
+  modalTextColor: "green",
+};
+
+const UsingReducer = () => {
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducerFunction, initialValue);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = {
+        id: new Date().getTime().toString(),
+        name,
+      };
+      dispatch({ type: "test", payLoad: newItem });
+    } else {
+      dispatch({ type: "noValue" });
+    }
+
+    setName("");
+  };
+  const closeModal = () => {
+    dispatch({ type: "closeModal" });
+  };
+  return (
+    <React.Fragment>
+      {state.modalOpen && (
+        <Modal
+          modalContent={state.modalContent}
+          modalTextColor={state.modalTextColor}
+          closeModal={closeModal}
+        />
+      )}
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="btn">Submit</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div className="item" key={person.id}>
+            <p>{person.name}</p>
+            <button
+              onClick={() => dispatch({ type: "remove", payLoad: person.id })}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default UsingReducer;
