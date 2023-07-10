dd8811fd79a8ea92adc715517d645b0b9a3f8195
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-07-10 11:24:59 +0530

    update

--- UseContext/usecontext/src/App.jsx
+++ UseContext/usecontext/src/App.jsx
@@ -1,47 +1,17 @@
-import React, { useState } from "react";
-import Modal from "./UseReducer/Modal";
-import { Data } from "./../src/Data";
+import React from "react";
+import UsingReducer from "./UseReducer/UsingReducer";
+import { Route, Routes } from "react-router-dom";
+import UsingState from "./UseReducer/UsingState";
+import Props from "./PropsDrilling/Props";
+
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [name, setName] = useState("");
-  const [people,setPeople]=useState(Data);
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    // const newPersons={...people,id:new Date().getTime().toString(),name}
-    // setPeople((people)=>{
-    //   return [...people,newPersons]
-    // }) // this method also works
-    setPeople([...people,{id:new Date().getTime().toString(),name}])
-    setName('')
-    if(name){
-      setShowModal(false)
-    }else{
-      setShowModal(true)
-    }
-  }
-  
   return (
     <React.Fragment>
-      {showModal && <Modal />}
-      <div>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button className="btn">Submit</button>
-        </form>
-        {
-          people.map((persons)=>{
-            return(
-              <div key={persons.id} className="item">
-                  <p>{persons.name}</p>
-              </div>
-            )
-          })
-        }
-      </div>
+      <Routes>
+        <Route path="state" element={<UsingState />} />
+        <Route path="reducer" element={<UsingReducer />} />
+        <Route path='props-drilling' element={<Props/>}/>
+      </Routes>
     </React.Fragment>
   );
 };
